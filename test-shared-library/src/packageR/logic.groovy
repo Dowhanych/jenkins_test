95eee532829c770d1a9446567b675124be73a704
Dowhanych <dowhanych@gmail.com>
2023-07-29 01:20:22 +0200

    add test library

---
+++ test-shared-library/src/packageR/logic.groovy
@@ -0,0 +1,9 @@
+// logic.groovy
+
+package packageR
+
+def pipelineStageLogic(data) {
+    // Your pipeline stage logic here
+    def processedData = 'Hello ' + data;
+    return processedData
+}
---
+++ test-shared-library/vars/superStep.groovy
@@ -0,0 +1,6 @@
+// superStep.groovy
+
+def call(data) {
+    // Your custom pipeline step logic here
+    echo "This is a custom pipeline step with data: ${data}"
+}
